b54f00f99f9cf676338f33666cac494ed1fcbfae
Nicholas Flynt <thegamefreak0134@gmail.com>
2018-08-10 18:56:23 -0500

    Add the base tilemaps that were used for testing the engine, for reference.
    Again, the tilemaps here are placeholder and will not be used in the final
    build. They're just for making sure the assembly isn't awful.

---
+++ art/SkullDungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SkullDungeon" tilewidth="16" tileheight="16" tilecount="36" columns="36">
+ <image source="skull_brick_wall.png" width="576" height="16"/>
+</tileset>
