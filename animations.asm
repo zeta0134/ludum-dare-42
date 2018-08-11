9d0d61ebc8e9135cdd42a32eb88e2b9c8e0be7fc
Nicholas Flynt <thegamefreak0134@gmail.com>
2018-08-11 04:14:12 -0500

    Write some very simple animations using the Blobby test sprite
    This demonstrates the data format we'd like to use for animations.
    Nothing too fancy, just keyframes with the tile index and an X/Y
    offset.

---
+++ animations.asm
@@ -0,0 +1,29 @@
+BlobbyBlobs:
+           ;Duration  Index  X-Offset  Y-Offset
+        DB 15,        0,        2,        0
+        DB 15,        1,        2,        0
+        DB  0,        0,        2,        0
+
+BlobbyJumps:
+        DB 60,        0,        0,        0
+        DB 4,         1,        0,       -1
+        DB 5,         1,        0,       -2
+        DB 6,         1,        0,       -3
+        DB 7,         1,        0,       -4
+        DB 10,        0,        0,       -5
+        DB 7,         1,        0,       -4
+        DB 6,         1,        0,       -3
+        DB 5,         1,        0,       -2
+        DB 4,         1,        0,       -1
+        DB 5,         0,        0,        0
+        DB 5,         1,        0,        0
+        DB 0,         0,        0,        0
+
+BlobbyWiggles:
+        DB 12,        0,        0,        0
+        DB 20,        0,        1,        0
+        DB 12,        0,        0,        0
+        DB 20,        0,       -1,        0
+        DB 0,         0,        0,        0
+
+
