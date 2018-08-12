867de249d33766710b33f558dc14a69af8736f7d
Nicholas Flynt <thegamefreak0134@gmail.com>
2018-08-11 21:28:27 -0500

    Remove Blobby remnants. Add item bobbing and MORE EXPLOSIONS.

--- animations.asm
+++ animations.asm
@@ -1,32 +1,5 @@
-BlobbyBlobs:
-           ;Duration  Index  X-Offset  Y-Offset
-        DB 15,        0,        2,        0
-        DB 15,        1,        2,        0
-        DB  0,        0,        2,        0
-
-BlobbyJumps:
-        DB 60,        0,        0,        0
-        DB 4,         1,        0,       -1
-        DB 5,         1,        0,       -2
-        DB 6,         1,        0,       -3
-        DB 7,         1,        0,       -4
-        DB 10,        0,        0,       -5
-        DB 7,         1,        0,       -4
-        DB 6,         1,        0,       -3
-        DB 5,         1,        0,       -2
-        DB 4,         1,        0,       -1
-        DB 5,         0,        0,        0
-        DB 5,         1,        0,        0
-        DB 0,         0,        0,        0
-
-BlobbyWiggles:
-        DB 12,        0,        0,        0
-        DB 20,        0,        1,        0
-        DB 12,        0,        0,        0
-        DB 20,        0,       -1,        0
-        DB 0,         0,        0,        0
-
 PlayerRuns:
+           ;Duration  Index  X-Offset  Y-Offset
         DB   5,       0,        0,       -1
         DB  10,       0,        0,       -2
         DB   5,       0,        0,       -1
@@ -38,3 +11,19 @@ PlayerRuns:
         DB   8,       4,        0,        0
         DB   8,       5,        0,        0
         DB   0,       0,        0,        0
+
+ItemBobs:
+           ;Duration  Index  X-Offset  Y-Offset
+        DB  20,       0,        0,        -1
+        DB  10,       0,        0,         0
+        DB  20,       0,        0,         1
+        DB  10,       0,        0,         0
+        DB   0,       0,        0,         0
+
+Explosion:
+           ;Duration  Index  X-Offset  Y-Offset
+        DB  15,       0,        0,         0
+        DB  15,       1,        0,         0
+        DB  15,       2,        0,         0
+        DB  15,       3,        0,         0
+        DB   0,       0,        0,         0
