95bf11f4d0a3e9cc6ad0f8f44b7118f776ea5fa4
Cristian Romo <cristian.g.romo@gmail.com>
2018-08-12 13:40:35 -0500

    Add score counting and display

--- hello_world.asm
+++ hello_world.asm
@@ -248,6 +248,7 @@ playerSpeedX: DS 1
 playerSpeedY: DS 1
 playerJumpTimer: DS 1
 playerAccelTimer: DS 1
+score: DS 3
 chunkBuffer: DS 256
 
 KEY_START EQU $80
@@ -299,9 +300,14 @@ updateChunks:
         ret
 
 initScore:
-        ld hl, shadowOAM + 34 * 4;
-        ld d, 5
-        ld a, 60
+        ld hl, score
+        ld a, 0
+        ld [hl+], a
+        ld [hl+], a
+        ld [hl], a
+        ld hl, shadowOAM + 34 * 4
+        ld d, 6
+        ld a, 63
 .loop
         ld [hl], 20 ; y-coordinate 
         inc hl
@@ -309,17 +315,75 @@ initScore:
         ld [hl], $50 ; tile-index
         inc hl
         inc hl ; skip over attributes
-        add a, 8
+        add a, 7
         dec d
         jp nz, .loop
         ret
 
 increaseScore:
-        ; TODO: this
+        ld hl, score + 2
+        ld a, [hl]
+        add a, 1
+        daa
+        ld [hl-], a
+        jp c, .hundreds
+        ret
+.hundreds
+        ld a, [hl]
+        add a, 1
+        daa
+        ld [hl-], a
+        jp c, .tenThousands
+        ret
+.tenThousands
+        ld a, [hl]
+        add a, 1
+        daa
+        ld [hl], a
         ret
 
 displayScore:
-        ; todo: this
+        ld hl, shadowOAM + 34 * 4 + 2
+        ld a, [score]
+        call displayScoreByte
+        ld a, [score + 1]
+        call displayScoreByte
+        ld a, [score + 2]
+        call displayScoreByte
+        ret
+
+; hl - address of first digit's tile attribute
+; a - BCD value to display
+; -> hl - address of third digit's tile attribute
+; -> a, b, c - trashed
+displayScoreByte:
+        ; save left digit in a, right digit in b
+        ld c, a
+        and a, %00001111
+        ld b, a
+        ld a, c
+        swap a
+        and a, %00001111
+
+        ; calculate and write tile
+        sla a
+        add a, $50
+        ld [hl], a
+        ; advance to next digit tile attribute
+        inc hl
+        inc hl
+        inc hl
+        inc hl
+        ld a, b
+        ; calculate and write tile
+        sla a
+        add a, $50
+        ld [hl], a
+        ; advance to next digit tile attribute
+        inc hl
+        inc hl
+        inc hl
+        inc hl
         ret
 
 updatePlayer:
