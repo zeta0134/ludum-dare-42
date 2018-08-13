109b14338dbd0971e7b8af22bc7fc67c8f38ef2b
Nicholas Flynt <thegamefreak0134@gmail.com>
2018-08-13 16:06:14 -0500

    Enable warning indicator for the explosion event horizon

--- warning.asm
+++ warning.asm
@@ -20,8 +20,70 @@ initWarningIndicators:
         ret
 
 updateWarningIndicators:
-        ; Note: explosion isn't implemented yet, so we don't do anything with slot 7
+        ; EXPLOSION INDICATOR
+        ; determine the distance between the player and the explosion horizon
+        ; How far away is the explosion from the player? Ignore sub speed here
+        ld hl, SpriteList + SPRITE_CHUNK
+        ld b, [hl]
+        ld hl, SpriteList + SPRITE_X_POS
+        ld c, [hl]
+        ; bc = player's chunk + x coordinate
+        ld hl, explosionChunk
+        ld d, [hl]
+        ld hl, explosionPos
+        ld e, [hl]
+        ; de = explosion chunk + x coordinate
+        ; 16-bit subtraction
+        ld a, c
+        sub a, e
+        ld e, a
+        ld a, b
+        sbc a, d
+        ld d, a
+        ;de = distance (positive is behing the player)
 
+        ld a, d ; a = chunks ahead of the explosion
+        ld de, Vanish
+        cp 3
+        jp nc, .checkAnimationLeft
+        ; we're at least 3 chunks out from blowing up.
+        ld de, FlashSlowly
+        cp 2
+        jp nc, .checkAnimationLeft
+        ; we're at least 2 chunks out from blowing up!
+        ld de, FlashQuickly
+        cp 1
+        jp nc, .checkAnimationLeft
+        ; we're just 1 chunks out from blowing up!!!!!
+        ld de, FlashIntensely
+        cp 0
+        jp nc, .checkAnimationLeft
+        ; we're ... well, we're dead. Oh well.
+        ld de, Vanish
+.checkAnimationLeft
+        ; check to see if our current animation is different from what we want to apply
+        ld a, 7
+        call getSpriteAddress ; sprite address in bc
+        ld hl, SPRITE_ANIMATION_START
+        add hl, bc
+        push de ;stash new animation
+        ld e, [hl]
+        inc hl
+        ld d, [hl] ; de contains current animation
+        pop bc ; bc contains new animation
+        ; subtract the high and low components, then OR the result
+        ld a, d
+        sub a, b
+        ld d, a
+        ld a, e
+        sub a, c
+        or d
+        ; if the result is zero, the original values match, so no need to update animation
+        jp z, .done
+        ld a, 7
+        call spawnSprite
+
+        ; END OF LEVEL INDICATOR
         ; for slot 8, determine the distance between the player and the death chunk
         ld hl, SpriteList + SPRITE_CHUNK
         ld b, [hl]
@@ -30,22 +92,22 @@ updateWarningIndicators:
         ; now, using the remaining chunks, we can select an intensity for the animation
         ld de, Vanish
         cp 9
-        jp nc, .checkAnimation
+        jp nc, .checkAnimationRight
         ; a is at least less than 8
         ld de, FlashSlowly
         cp 5
-        jp nc, .checkAnimation
+        jp nc, .checkAnimationRight
         ; a is at least less than 4
         ld de, FlashQuickly
         cp 3
-        jp nc, .checkAnimation
+        jp nc, .checkAnimationRight
         ; a is at least less than 2
         ld de, FlashIntensely
         cp 1
-        jp nc, .checkAnimation
+        jp nc, .checkAnimationRight
         ;The player is dead, so _hide_ the animation
         ld de, Vanish
-.checkAnimation
+.checkAnimationRight
         ; check to see if our current animation is different from what we want to apply
         ld a, 8
         call getSpriteAddress ; sprite address in bc
